bf30ec1325a7bee6a9cc2e759f7eb4487b2a2ddc
Priyanka <singhpri1603@gmail.com>
2018-04-27 15:03:13 -0500

    Emit newMessage in createMessage
    Added newMessage in createMessage for broadcast

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Pri',
-        text: 'Hey Pri'
-    });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath)); //to serve the index.html page
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Me',
-        text: 'Hey Pri',
-        createdAt: 123
-    });
-
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
 
     socket.on('disconnect', () => {
