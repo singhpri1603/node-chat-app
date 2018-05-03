d6134ed0fb15a5637bb436843cee46f94e1355a4
Priyanka <singhpri1603@gmail.com>
2018-05-03 16:55:07 -0500

    Added geolocation messages

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -11,4 +11,18 @@ describe('generateMessage', () => {
         expect(typeof message.createdAt).toBe('number');
         expect.objectContaining({from, text});
     });  
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'Sahil';
+        var lat = 26;
+        var long = 45;
+        var url = 'https://google.com/maps?q=26,45';
+
+        var message = generateLocationMessage(from, lat, long);
+
+        expect(typeof message.createdAt).toBe('number');
+        expect.objectContaining({from, url});
+    });
 });
